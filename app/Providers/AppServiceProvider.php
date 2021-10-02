d566eabc646c6eb1b03aa5c6105952ff96d96c0d
jardelferreira <jardelferreira.sousa@gmail.com>
2021-10-02 11:19:09 -0300

    Definindo tamanho maximo para string MSQL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use NascentAfrica\Jetstrap\JetstrapFacade;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JetstrapFacade::useAdminLte3();
+        Schema::defaultStringLength(125);
     }
 }
